8c53df92641bbc739fa3d4ec4f3447dfd3993cf3
teeshkeen <iteeshkeen@yandex.ru>
2025-08-24 19:44:28 +0500

    test: fail test for throw ball and add score method

--- tests/bowling_tests.cpp
+++ tests/bowling_tests.cpp
@@ -9,3 +9,9 @@ TEST(BowlingGameTest, NewGameZeroScore){
     BowlingGame game;
     EXPECT_EQ(game.getScore(), 0);
 }
+
+TEST(BowlingGameTest, ThrowAndAddScore) {
+    BowlingGame game;
+    game.throwBall(5);
+    EXPECT_EQ(game.getScore(), 5);
+}
